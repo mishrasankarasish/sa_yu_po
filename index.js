4644bb8bedd6669c24d53aedef36932e9905b09e
mishrasankarasish <140536644+mishrasankarasish@users.noreply.github.com>
2025-05-04 23:16:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello MCKV');
-});
-
-app.get('/courses', (req, res) => {
-  res.json([
-    { id: 1, name: 'Mathematics' },
-    { id: 2, name: 'Computer Science' },
-    { id: 3, name: 'Electronics' }
-  ]);
-});
-
-app.listen(4000, () => {
-  console.log("Server started on port 4000");
-});
+// hello.js
+console.log("Hello World");
