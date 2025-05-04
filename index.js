7b27adf8af0663e22c2fdfdd1a414ceef642351d
mishrasankarasish <140536644+mishrasankarasish@users.noreply.github.com>
2025-05-04 23:18:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-// hello.js
-console.log("Hello World");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(4000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
