f6afa230264bc4ec09208d48c5d3d0b9c470a7e3
mishrasankarasish <140536644+mishrasankarasish@users.noreply.github.com>
2025-05-04 23:08:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV')
-})
+  res.send('Hello MCKV');
+});
 
 app.get('/courses', function (req, res) {
-  res.send('Print all courses')
-})
+  res.send('Print all courses');
+});
 
-app.listen(4000, ()=>{
-    console.log("Started server");
-}
-)
+app.listen(4000, () => {
+  console.log("Started server");
+});
